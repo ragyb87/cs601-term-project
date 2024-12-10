2b053e3b87bab98a16d0c9ad3de286153ea6da4d
ragyb87 <ragybasilious@gmail.com>
2024-12-09 16:57:45 -0800

    fix URL

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,21 +2,9 @@ import React from "react";
 import Footer from "../components/FooterInclude";
 
 function Home() {
-
-  (function(l) {
-    if (l.search[1] === '/' ) {
-      var decoded = l.search.slice(1).split('&').map(function(s) { 
-        return s.replace(/~and~/g, '&')
-      }).join('?');
-      window.history.replaceState(null, null,
-          l.pathname.slice(0, -1) + decoded + l.hash
-      );
-    }
-  }(window.location))
-
   return (
     <>
-    <div id="root"></div>
+    
     <div className="home">
       <div className="home-content">
         <div className="text-section">
@@ -31,7 +19,7 @@ function Home() {
             I'm passionate about creating innovative web solutions and delivering impactful projects.
             If you're seeking someone with the skills to bring ideas to life, let's connect!
           </p>
-         { /* <a href="/portfolio" className="cta-button">Explore My Work</a> */}
+          { /*<a href="/portfolio" className="cta-button">Explore My Work</a> */}
         </div>
         <div className="image-section">
           <img src="cs601-term-project/assets/person.webp" alt="Ragy Basilious" className="profile-image" />
